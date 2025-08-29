4cdef07051ef58447f836a8aced45d19bbd67721
lbk209 <80578322+lbk209@users.noreply.github.com>
2025-08-29 18:17:51 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import SearchPage from './SearchPage.jsx'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SearchPage />
-  </React.StrictMode>,
+<React.StrictMode>
+<SearchPage />
+</React.StrictMode>,
 )
